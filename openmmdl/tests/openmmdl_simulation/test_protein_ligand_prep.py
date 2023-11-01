2f88359cc812e069d5e40992dd34d0e693e2f616
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-01 23:00:28 +0100

    Update test_protein_ligand_prep.py

--- openmmdl/tests/openmmdl_simulation/test_protein_ligand_prep.py
+++ openmmdl/tests/openmmdl_simulation/test_protein_ligand_prep.py
@@ -18,6 +18,8 @@ test_data_directory = Path("openmmdl/tests/data/in")
 
 # Define the full path to the input SDF file
 TEST_LIGAND_FILE = f"{test_data_directory}/CVV.sdf"
+TEST_MOL_FILE = f"{test_data_directory}/CVV.mol"
+TEST_MOL2_FILE = f"{test_data_directory}/CVV.mol2"
 TEST_PROTEIN = test_data_directory / '6b73.pdb'
 
 # Test the protein_choice function
@@ -28,10 +30,14 @@ def test_protein_choice():
 # Test the prepare_ligand function
 def test_prepare_ligand():
     # Test the function with the sample ligand file.
-    rdkit_mol = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=True)
+    rdkit_mol_sdf = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=True)
+    rdkit_mol_mol = prepare_ligand(TEST_MOL_FILE, minimize_molecule=False)
+    rdkit_mol_mol2 = prepare_ligand(TEST_MOL2_FILE, minimize_molecule=False)
     
     # Add your assertions here to check if the preparation worked as expected
-    assert rdkit_mol is not None  # Check if the result is not None
+    assert rdkit_mol_sdf is not None  # Check if the result is not None
+    assert rdkit_mol_mol is not None  # Check if the result is not None
+    assert rdkit_mol_mol2 is not None  # Check if the result is not None
 
 if __name__ == '__main__':
     pytest.main()
