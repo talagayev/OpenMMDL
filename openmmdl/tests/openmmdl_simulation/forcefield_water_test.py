b48cac8de6549c328ea1a6979a99d3a8e37e12d2
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-01 21:24:23 +0100

    Update forcefield_water_test.py

--- openmmdl/tests/openmmdl_simulation/forcefield_water_test.py
+++ openmmdl/tests/openmmdl_simulation/forcefield_water_test.py
@@ -34,6 +34,17 @@ def test_generate_forcefield(sample_rdkit_molecule):
     forcefield = generate_forcefield('amber14-all.xml', 'amber14/tip3p.xml', False, sample_rdkit_molecule)
     assert isinstance(forcefield, app.ForceField)
 
+@pytest.mark.parametrize("add_membrane, protein_ff, expected_ff", [
+    (True, 'amber10.xml', ['amber10.xml', 'amber14/lipid17.xml']),
+    (True, 'amber99sb.xml', ['amber99sb.xml', 'amber14/lipid17.xml']),
+    (False, 'amber14-all.xml', ['amber14-all.xml', 'amber14/tip3p.xml']),
+    (False, 'amber03.xml', ['amber03.xml', 'amber14/tip3p.xml']),
+])
+def test_generate_forcefield_membrane_logic(add_membrane, protein_ff, expected_ff):
+    forcefield = generate_forcefield(protein_ff, 'amber14/tip3p.xml', add_membrane, sample_rdkit_molecule)
+    ff_files = [ff._forcefield for ff in forcefield._forces]
+    assert ff_files == expected_ff
+
 def test_generate_transitional_forcefield(sample_rdkit_molecule):
     transitional_forcefield = generate_transitional_forcefield('amber14-all.xml', 'tip3p.xml', True, sample_rdkit_molecule)
     assert isinstance(transitional_forcefield, app.ForceField)
