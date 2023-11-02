04c2ba6b45c8c6766749a0142ea15b570ab79a60
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-02 22:00:51 +0100

    Update test_rmsd_calculation.py

--- openmmdl/tests/openmmdl_analysis/test_rmsd_calculation.py
+++ openmmdl/tests/openmmdl_analysis/test_rmsd_calculation.py
@@ -3,3 +3,35 @@ import pytest
 import pandas as pd
 import numpy as np
 import mdtraj as md
+
+from openmmdl.openmmdl_analysis.rmsd_calculation import rmsd_for_atomgroups
+
+test_data_directory = Path("openmmdl/tests/data/in")
+topology_file = "0_unk_hoh.pdb"
+trajectory_file = "all_50.dcd"
+selection1 = "protein"
+selection2 = ["ligand"]
+
+def test_rmsd_for_atomgroups():
+
+    # Go into folder with data
+    original_cwd = os.getcwd()
+    os.chdir(test_data_directory)
+
+    # Call the function
+    rmsd_df = rmsd_for_atomgroups(topology_file, trajectory_file, selection1, selection2)
+
+    # Check if the output DataFrame has the correct structure
+    assert isinstance(rmsd_df, pd.DataFrame)
+    assert rmsd_df.index.name == "frame"
+    
+    # Check if the CSV file exists
+    assert os.path.exists("RMSD_over_time.csv")
+    
+    # Check if the plot file exists
+    assert os.path.exists("RMSD_over_time.png")
+    
+    # Cleanup created files after the test
+    os.remove("RMSD_over_time.csv")
+    os.remove("RMSD_over_time.png")
+    os.chdir(original_cwd)
