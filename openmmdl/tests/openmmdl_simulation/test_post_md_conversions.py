5464e27c9e1f06709ce3463ed104471422c363ef
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-02 19:19:47 +0100

    Update test_post_md_conversions.py

--- openmmdl/tests/openmmdl_simulation/test_post_md_conversions.py
+++ openmmdl/tests/openmmdl_simulation/test_post_md_conversions.py
@@ -1,23 +1,21 @@
 import pytest
 import os
+from pathlib import Path
 import mdtraj as md
 
 from openmmdl.openmmdl_simulation.scripts.post_md_conversions import mdtraj_conversion, MDanalysis_conversion
 
 
-pdb_file = "0_unk_hoh.pdb"
-test_data_dir = "openmmdl/tests/data/in"
 
+test_data_directory = Path("openmmdl/tests/data/in")
+pdb_file = f"{test_data_directory}/0_unk_hoh.pdb"
 
 def test_mdtraj_conversion():
     # Create temporary directories to save the output files
-    output_file_dcd = 'centered_old_coordinates.dcd'
+    output_file_dcd = f"{test_data_directory}/0_unk_hoh.pdb"
     output_file_xtc = 'centered_old_coordinates.xtc'
     output_file_pdb = 'centered_old_coordinates_top.pdb'
     output_file_gro = 'centered_old_coordinates_top.gro'
-    
-    original_cwd = os.getcwd()
-    os.chdir(test_data_dir)
   
     mdtraj_conversion(pdb_file, "gro_xtc")
     mdtraj_conversion(pdb_file, "pdb_dcd")
@@ -27,4 +25,3 @@ def test_mdtraj_conversion():
     assert output_file_pdb is not None
     assert output_file_gro is not None
 
-    os.chdir(original_cwd)
