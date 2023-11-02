aec3b75ff7adfbffa7c08047d411b0024ed2e55e
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-02 01:17:19 +0100

    Update ligand_processing_test.py

--- openmmdl/tests/openmmdl_analysis/ligand_processing_test.py
+++ openmmdl/tests/openmmdl_analysis/ligand_processing_test.py
@@ -1,7 +1,11 @@
 import pytest
 import openmmdl
+from pathlib import Path
 from openmmdl.openmmdl_analysis.ligand_processing import increase_ring_indices, convert_ligand_to_smiles
 
+
+TEST_LIGAND_FILE = f"{test_data_directory}/CVV.sdf"
+TEST_OUTPUT_FILE = "CVV.smi"
 
 def test_increase_ring_indices():
     # Test case 1: Check if ring indices are correctly increased
@@ -16,7 +20,20 @@ def test_increase_ring_indices():
     result = increase_ring_indices(ring, lig_index)
     assert result == [23, 24, 25]
 
-    # Add more test cases as needed
+
+def test_convert_ligand_to_smiles():
+    # Convert the ligand structure to SMILES in the same directory as the input SDF file
+    convert_ligand_to_smiles(TEST_LIGAND_FILE, TEST_OUTPUT_FILE)
+
+    # Verify that the output SMILES file was created in the same directory as the input file
+    assert os.path.exists(TEST_OUTPUT_FILE)
+
+    # Optionally, you can also read and validate the content of the output SMILES file
+    with open(TEST_OUTPUT_FILE, "r") as smi_file:
+        smiles_lines = smi_file.readlines()
+        assert len(smiles_lines) > 0  # Check that there are SMILES representations
+
+
 
 if __name__ == '__main__':
     pytest.main()
