d3c5368099d78cc891010caa96bcf7d08ee755f8
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-02 03:22:54 +0100

    Update test_protein_ligand_prep.py

--- openmmdl/tests/openmmdl_simulation/test_protein_ligand_prep.py
+++ openmmdl/tests/openmmdl_simulation/test_protein_ligand_prep.py
@@ -39,7 +39,7 @@ def test_protein_choice():
 # Test the prepare_ligand function
 def test_prepare_ligand():
     # Test the function with the sample ligand file.
-    rdkit_mol_sdf = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=True)
+    rdkit_mol_sdf = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=False)
     rdkit_mol_mol = prepare_ligand(TEST_MOL_FILE, minimize_molecule=False)
     rdkit_mol_mol2 = prepare_ligand(TEST_MOL2_FILE, minimize_molecule=False)
     
@@ -47,43 +47,6 @@ def test_prepare_ligand():
     assert rdkit_mol_sdf is not None  # Check if the result is not None
     assert rdkit_mol_mol is not None  # Check if the result is not None
     assert rdkit_mol_mol2 is not None  # Check if the result is not None
-
-
-
-def test_rdkit_to_openmm_conversion():
-    rdkit_mol = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=False)
-    
-    off_mol = Molecule.from_rdkit(rdkit_mol)
-
-    # add name for molecule
-    off_mol.name = name
-
-    # add names for atoms
-    element_counter_dict = {}
-    for off_atom, rdkit_atom in zip(off_mol.atoms, rdkit_mol.GetAtoms()):
-        element = rdkit_atom.GetSymbol()
-        if element in element_counter_dict.keys():
-            element_counter_dict[element] += 1
-        else:
-            element_counter_dict[element] = 1
-        off_atom.name = element + str(element_counter_dict[element])
-        
-    # convert from OpenFF to OpenMM
-    off_mol_topology = off_mol.to_topology()
-    mol_topology = off_mol_topology.to_openmm()
-    mol_positions = off_mol.conformers[0];
-    new_mol_positions = []
-
-    # convert units from Ångström to Nanometers
-    for mol_position in off_mol.conformers[0]:
-        new_mol_positions.append(mol_position.magnitude/10.0);
-        print("yep")
-
-    # combine topology and positions in modeller object
-    omm_mol = app.Modeller(mol_topology, new_mol_positions * unit.nanometers)
-    
-    # Check if the OpenMM modeller is an instance of OpenMM's app.Modeller
-    assert omm_mol is not None
 
 
 if __name__ == '__main__':
