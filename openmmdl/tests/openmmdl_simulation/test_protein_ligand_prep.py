7af021a1dbf1376406e53b148347aab1a6067cd7
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-02 14:20:41 +0100

    Add files via upload

---
+++ openmmdl/tests/openmmdl_simulation/test_protein_ligand_prep.py
@@ -0,0 +1,88 @@
+import pytest
+import os
+import rdkit
+from rdkit import Chem
+import simtk.openmm.app as app
+from simtk.openmm.app import PDBFile, Modeller
+from simtk.openmm import unit
+from simtk.openmm import Vec3
+import mdtraj as md
+import numpy as np
+from pathlib import Path
+import pdbfixer
+from openmm.app import PDBFile
+from pdbfixer import PDBFixer
+import simtk.openmm.app as app
+
+
+from simtk.openmm.app import PDBFile, Modeller, PDBReporter, StateDataReporter, DCDReporter, CheckpointReporter
+from simtk.openmm import unit, Platform, MonteCarloBarostat, LangevinMiddleIntegrator
+from simtk.openmm import Vec3
+import simtk.openmm as mm
+
+
+from openmmdl.openmmdl_simulation.scripts.forcefield_water import ff_selection, water_forcefield_selection, water_model_selection, generate_forcefield, generate_transitional_forcefield
+from openmmdl.openmmdl_simulation.scripts.protein_ligand_prep import protein_choice, prepare_ligand, rdkit_to_openmm, merge_protein_and_ligand, water_padding_solvent_builder, water_absolute_solvent_builder, membrane_builder, water_conversion
+from openmmdl.openmmdl_simulation.scripts.post_md_conversions import mdtraj_conversion, MDanalysis_conversion
+
+
+protein = "6b73.pdb"
+ligand = 'CVV.sdf'
+ligand_name = "UNK"
+minimization = False
+sanitization = False
+protein_prepared = "Yes"
+ff = 'AMBER14'
+water = 'TIP3P-FB'
+add_membrane = False
+Water_Box = "Buffer"
+water_padding_distance = 1.0
+water_boxShape = 'cube'
+water_ionicstrength = 0.15
+water_positive_ion = 'Na+'
+water_negative_ion = 'Cl-'
+
+
+# Print current working directory
+print("Current working directory:", os.getcwd())
+
+# Assuming that 'test_data_directory' is properly defined in your test setup
+test_data_directory = "openmmdl/tests/data/in"
+
+
+test_data_directory = Path("openmmdl/tests/data/in")
+
+
+# Define the full path to the input SDF file
+TEST_LIGAND_FILE = f"{test_data_directory}/CVV.sdf"
+TEST_MOL_FILE = f"{test_data_directory}/CVV.mol"
+TEST_MOL2_FILE = f"{test_data_directory}/CVV.mol2"
+TEST_PROTEIN = test_data_directory / '6b73.pdb'
+
+
+ligand_prepared = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=False)
+
+# Test the protein_choice function
+def test_protein_choice():
+    prepared_protein = protein_choice("Yes", TEST_PROTEIN)
+    assert isinstance(prepared_protein, pdbfixer.PDBFixer)
+
+# Test the prepare_ligand function
+def test_prepare_ligand():
+    # Test the function with the sample ligand file.
+    rdkit_mol_sdf = prepare_ligand(TEST_LIGAND_FILE, minimize_molecule=False)
+    rdkit_mol_mol = prepare_ligand(TEST_MOL_FILE, minimize_molecule=False)
+    rdkit_mol_mol2 = prepare_ligand(TEST_MOL2_FILE, minimize_molecule=False)
+    
+    # Add your assertions here to check if the preparation worked as expected
+    assert rdkit_mol_sdf is not None  # Check if the result is not None
+    assert rdkit_mol_mol is not None  # Check if the result is not None
+    assert rdkit_mol_mol2 is not None  # Check if the result is not None
+
+def test_rdkit_to_openmm():
+    omm_ligand = rdkit_to_openmm(ligand_prepared, ligand_name)
+    assert isinstance(omm_ligand, simtk.openmm.app.Modeller)
+
+
+if __name__ == '__main__':
+    pytest.main()
