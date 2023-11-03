d88a4e929cfdb3f6a44cd68b5748b5d602720ad8
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-03 16:11:13 +0100

    Update test_rdkit_figure_generation.py

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 from pathlib import Path
-from openmmdl.openmmdl_analysis.rdkit_figure_generation import split_interaction_data
+from openmmdl.openmmdl_analysis.rdkit_figure_generation import split_interaction_data, highlight_numbers
 
 
 @pytest.mark.parametrize("input_data, expected_output", [
@@ -13,23 +13,33 @@ def test_split_interaction_data(input_data, expected_output):
     result = split_interaction_data(input_data)
     assert result == expected_output
 
+def test_highlight_numbers():
+    # Input data
+    split_data = [
+        "163GLYA 4202 Acceptor hbond",
+        "165ASPA 4203 Donor hbond",
+        "165ASPA 4222 Donor hbond",
+        "165ASPA 4203 Acceptor hbond",
+        "125TYRA 4192 Acceptor waterbridge",
+        "161PHEA 4211 4212 4213 4214 4215 4210 hydrophobic",
+        "59ARGA 4205 4206 4207 4216 4217 4218 Aromatic pication",
+        "98PHEA 4210,4211,4212,4213,4214,4215 pistacking",
+        "59ARGA 4194 F halogen",
+        "166ARGA 4202,4203 Carboxylate NI saltbridge"
+    ]
 
-# Define test cases
-@pytest.mark.parametrize("split_data, starting_idx, expected_output_highlight", [
-    (["Res1_TypeA 1 2 3 HydrogenBond", "Res2_TypeB 4 5 6 HydrogenBond"], 1, ([0], [1, 2], [], [], [], [], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 3 HydrogenBond Donor", "Res2_TypeB 4 5 6 HydrogenBond Acceptor"], 1, ([0], [1], [2], [], [], [], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 3 HydrogenBond Donor", "Res2_TypeB 4 5 6 HydrogenBond Donor"], 1, ([0, 1], [], [], [], [], [], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 Hydrophobic", "Res2_TypeB 4 5 Hydrophobic"], 1, ([], [], [], [0, 1], [], [], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 3 WaterBridge", "Res2_TypeB 4 5 WaterBridge"], 1, ([], [], [], [], [0, 1], [], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 3 Pistacking", "Res2_TypeB 4 5 6 Pistacking"], 1, ([], [], [], [], [], [0, 1, 2], [], [], [], [], [])),
-    (["Res1_TypeA 1 2 3 Halogen HalogenType", "Res2_TypeB 4 5 6 Halogen HalogenType"], 1, ([], [], [], [], [], [], [0, 1, 2], [], [], [], [])),
-    (["Res1_TypeA 1,2 3 SaltBridge PI", "Res2_TypeB 4,5 SaltBridge NI"], 1, ([], [], [], [], [], [], [], [0, 1], [2, 3], [], [])),
-    (["Res1_TypeA 1 2 Pication HalogenType", "Res2_TypeB 4 5 Pication HalogenType"], 1, ([], [], [], [], [], [], [], [], [], [0, 1, 2], [])),
-    (["Res1_TypeA 1 2 3 Metal HalogenType", "Res2_TypeB 4 5 6 Metal HalogenType"], 1, ([], [], [], [], [], [], [], [], [], [], [0, 1, 2, 3])),
-])
-def test_highlight_numbers(split_data, starting_idx, expected_output_highlight):
-    result = highlight_numbers(split_data, starting_idx)
-    assert result == expected_output_highlight
+    starting_idx = 1  # Updated starting index
+
+    assert highlighted_hbond_donor is not None
+    assert highlighted_hbond_acceptor is not None
+    assert highlighted_hbond_both is not None
+    assert highlighted_hydrophobic is not None
+    assert highlighted_waterbridge is not None
+    assert highlighted_pistacking is not None
+    assert highlighted_halogen is not None
+    assert highlighted_ni is not None
+    assert highlighted_pication is not None
+    
 
 # Run the tests
 if __name__ == "__main__":
