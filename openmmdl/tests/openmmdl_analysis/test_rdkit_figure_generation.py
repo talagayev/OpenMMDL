9948e5a1ab8ae1144033a0463e14703a44ca9939
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-03 14:49:58 +0100

    Update test_rdkit_figure_generation.py

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -15,6 +15,24 @@ def test_split_interaction_data(input_data, expected_output):
     result = split_interaction_data(input_data)
     assert result == expected_output
 
+
+# Define test cases
+@pytest.mark.parametrize("split_data, starting_idx, expected_output_highlight", [
+    (["Res1_TypeA 1 2 3 HydrogenBond", "Res2_TypeB 4 5 6 HydrogenBond"], 1, ([0], [1, 2], [], [], [], [], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 3 HydrogenBond Donor", "Res2_TypeB 4 5 6 HydrogenBond Acceptor"], 1, ([0], [1], [2], [], [], [], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 3 HydrogenBond Donor", "Res2_TypeB 4 5 6 HydrogenBond Donor"], 1, ([0, 1], [], [], [], [], [], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 Hydrophobic", "Res2_TypeB 4 5 Hydrophobic"], 1, ([], [], [], [0, 1], [], [], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 3 WaterBridge", "Res2_TypeB 4 5 WaterBridge"], 1, ([], [], [], [], [0, 1], [], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 3 Pistacking", "Res2_TypeB 4 5 6 Pistacking"], 1, ([], [], [], [], [], [0, 1, 2], [], [], [], [], [])),
+    (["Res1_TypeA 1 2 3 Halogen HalogenType", "Res2_TypeB 4 5 6 Halogen HalogenType"], 1, ([], [], [], [], [], [], [0, 1, 2], [], [], [], [])),
+    (["Res1_TypeA 1,2 3 SaltBridge PI", "Res2_TypeB 4,5 SaltBridge NI"], 1, ([], [], [], [], [], [], [], [0, 1], [2, 3], [], [])),
+    (["Res1_TypeA 1 2 Pication HalogenType", "Res2_TypeB 4 5 Pication HalogenType"], 1, ([], [], [], [], [], [], [], [], [], [0, 1, 2], [])),
+    (["Res1_TypeA 1 2 3 Metal HalogenType", "Res2_TypeB 4 5 6 Metal HalogenType"], 1, ([], [], [], [], [], [], [], [], [], [], [0, 1, 2, 3])),
+])
+def test_highlight_numbers(split_data, starting_idx, expected_output_highlight):
+    result = highlight_numbers(split_data, starting_idx)
+    assert result == expected_output_highlight
+
 # Run the tests
 if __name__ == "__main__":
     pytest.main()
