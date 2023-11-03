b248a8d5776221d1e0721198f974acc6d6ae03fc
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-03 01:58:34 +0100

    Update test_interaction_gathering.py

--- openmmdl/tests/openmmdl_analysis/test_interaction_gathering.py
+++ openmmdl/tests/openmmdl_analysis/test_interaction_gathering.py
@@ -6,12 +6,13 @@ import numpy as np
 import mdtraj as md
 from plip.structure.preparation import PDBComplex, LigandFinder, Mol, PLInteraction
 
-from openmmdl.openmmdl_analysis.interaction_gathering import characterize_complex
+from openmmdl.openmmdl_analysis.interaction_gathering import characterize_complex, retrieve_plip_interactions
 
 
 test_data_directory = Path("openmmdl/tests/data/in")
 topology_file = f"{test_data_directory}/complex.pdb"
 binding_site_id = "UNK:X:0"
+lig_name = "UNK"
 
 # Test the function
 def test_characterize_complex():
@@ -22,7 +23,11 @@ def test_characterize_complex():
     # Check if the function returns a PLInteraction object
     assert isinstance(interaction_set, PLInteraction)
 
-    # Additional test cases and assertions can be added to test different scenarios.
-    
-    # Clean up created files after the test
-    os.remove(pdb_file)
+def test_retrieve_plip_interactions(prepare_test_data):
+    pdb_file, ligand_name = prepare_test_data
+
+    # Call the function
+    interactions = retrieve_plip_interactions(topology_file, lig_name)
+
+    # Check if the function returns a dictionary
+    assert isinstance(interactions, dict)
