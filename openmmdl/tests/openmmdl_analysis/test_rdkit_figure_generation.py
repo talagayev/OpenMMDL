fc19356fd1164e1140bcb2cd4f8f91a23e6c5dce
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-03 22:09:02 +0100

    Update test_rdkit_figure_generation.py

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -46,22 +46,22 @@ def test_highlight_numbers():
     
 def test_update_dict():
     # Test case 1: Check if the target dictionary is updated correctly
-    target_dict = {'1': 'one', '2': 'two'}
-    source_dict = {'3': 'three', '4': 'four'}
+    target_dict = {'1': '1', '2': '2'}
+    source_dict = {'3': '3', '4': '4'}
     update_dict(target_dict, source_dict)
-    assert target_dict == {1: 'one', 2: 'two', 3: 'three', 4: 'four'}
+    assert target_dict == {1: '1', 2: '2', 3: '3', 4: '4'}
 
     # Test case 2: Check if the function handles multiple source dictionaries
     target_dict = {}
-    source_dict1 = {'1': 'one'}
-    source_dict2 = {'2': 'two', '3': 'three'}
+    source_dict1 = {'1': '1'}
+    source_dict2 = {'2': '2', '3': '3'}
     update_dict(target_dict, source_dict1, source_dict2)
-    assert target_dict == {1: 'one', 2: 'two', 3: 'three'}
+    assert target_dict == {1: 1, 2: 2, 3: 3}
 
     # Test case 3: Check if the function handles empty source dictionaries
-    target_dict = {'1': 'one', '2': 'two'}
+    target_dict = {'1': '1', '2': 'tw2o'}
     update_dict(target_dict)  # No source dictionaries provided
-    assert target_dict == {1: 'one', 2: 'two'}
+    assert target_dict == {1: '1', 2: '2'}
 
 
 
