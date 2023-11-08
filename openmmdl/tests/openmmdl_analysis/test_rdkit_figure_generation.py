f5e1cced1a7c085252bfb5fc2e1657652641c33b
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-08 01:26:42 +0100

    Update test_rdkit_figure_generation.py

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -151,7 +151,7 @@ def test_arranged_figure_generation():
     arranged_figure_generation(merged_image_paths, output_path)
     print(output_path)
 
-    ## Print the current files in the working directory for debugging
+    # Print the current files in the working directory for debugging
     files_in_working_directory = os.listdir(working_directory)
     print("Files in Working Directory:", files_in_working_directory)
 
