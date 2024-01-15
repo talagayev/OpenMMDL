99fa11252c8a424654dda67367aefd68e943439c
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-15 12:39:45 +0100

    Update cleaning_procedures_test.py

--- openmmdl/tests/openmmdl_simulation/cleaning_procedures_test.py
+++ openmmdl/tests/openmmdl_simulation/cleaning_procedures_test.py
@@ -141,6 +141,45 @@ def test_post_md_file_movement():
     assert os.path.exists(checkpoints_dir / "100x_checkpoint.chk")
 
 
+def test_post_md_file_movement_2():
+    # Get the absolute path to the test data directory
+    test_data_directory = Path("openmmdl/tests/data/in")
+
+    # Define the full path to the input files
+    ligand = test_data_directory / 'CVV.sdf'
+    protein_name = test_data_directory / '6b73.pdb'
+    prmtop = test_data_directory / '6b73.prmtop'
+    inpcrd = test_data_directory / '6b73.inpcrd'
+    
+    # Assert that the input files exist before moving
+    assert os.path.exists(ligand)
+    assert os.path.exists(protein_name)
+    assert os.path.exists(prmtop)
+    assert os.path.exists(inpcrd)
+
+    # Mock the existence of the prepared_no_solvent_6b73.pdb file
+    with patch('os.path.exists', return_value=True):
+        # Call the post_md_file_movement function
+        post_md_file_movement(str(protein_name), str(prmtop), str(inpcrd), [str(ligand)])
+    
+        # Check if the files have been organized and moved to the correct directories
+        input_files_dir = Path("Input_Files")
+        md_files_dir = Path("MD_Files")
+        md_postprocessing_dir = Path("MD_Postprocessing")
+        final_output_dir = Path("Final_Output")
+        checkpoints_dir = Path("Checkpoints")
+
+        assert os.path.exists(input_files_dir)
+        assert os.path.exists(md_files_dir / "Pre_MD" / "prepared_no_solvent_6b73.pdb")
+        assert os.path.exists(md_files_dir / "Minimization_Equilibration" / "Energyminimization_6b73.pdb")
+        assert os.path.exists(md_files_dir / "MD_Output" / "output_6b73")
+        assert os.path.exists(md_postprocessing_dir / "centered_old_coordinates_top.pdb")
+        assert os.path.exists(final_output_dir / "All_Atoms" / "centered_top.pdb")
+        assert os.path.exists(final_output_dir / "Prot_Lig" / "prot_lig_top.pdb")
+        assert os.path.exists(checkpoints_dir / "checkpoint.chk")
+        assert os.path.exists(checkpoints_dir / "10x_checkpoint.chk")
+        assert os.path.exists(checkpoints_dir / "100x_checkpoint.chk")
+
 
 
 # Run the tests
