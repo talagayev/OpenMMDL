03e75f10b629186b22309d74bbbb7f9771facfc6
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-15 14:24:08 +0100

    Update test_post_md_conversions.py

--- openmmdl/tests/openmmdl_simulation/test_post_md_conversions.py
+++ openmmdl/tests/openmmdl_simulation/test_post_md_conversions.py
@@ -12,7 +12,7 @@ pdb_file = "0_unk_hoh.pdb"
 dcd_file = "trajectory.dcd"
 ligand_name = 'UNK'
 
-def test_MDanalysis_conversion():
+def test_mdtraj_conversion():
     original_cwd = os.getcwd()
     os.chdir(test_data_directory)
     # Create temporary directories to save the output files
@@ -30,27 +30,35 @@ def test_MDanalysis_conversion():
     assert output_file_gro is not None
     os.chdir(original_cwd)
 
-def test_mdtraj_conversion():
+def test_mdanalysis_conversion():
     original_cwd = os.getcwd()
     os.chdir(test_data_directory)
     
     # Create temporary directories to save the output files
     all_file_dcd = "centered_traj.dcd"
+    all_file_dcd_unaligned = "centered_traj_unaligned.dcd"
     all_file_pdb = 'centered_top.pdb'
     prot_lig_file_dcd = 'prot_lig_traj.dcd'
+    prot_lig_file_dcd_unaligned = 'prot_lig_traj_unaligned.dcd'
     prot_lig_file_pdb = 'prot_lig_top.pdb'
     all_file_xtc = "centered_traj.xtc"
+    all_file_xtc_unaligned = "centered_traj_unaligned.xtc"
     all_file_gro = "centered_top.gro"
     prot_lig_file_xtc = 'prot_lig_traj.xtc'
+    prot_lig_file_xtc_unaligned = 'prot_lig_traj_unaligned.xtc'
     prot_lig_file_gro = 'prot_lig_top.gro'
 
     MDanalysis_conversion(pdb_file, dcd_file, ligand_name, "pdb_dcd_gro_xtc", "mda_prot_lig_all")
 
     assert all_file_dcd is not None
+    assert all_file_dcd_unaligned is not None
     assert all_file_pdb is not None
     assert prot_lig_file_dcd is not None
+    assert prot_lig_file_dcd_unaligned is not None
     assert prot_lig_file_pdb is not None
     assert all_file_xtc is not None
+    assert all_file_xtc_unaligned is not None
     assert all_file_gro is not None
     assert prot_lig_file_xtc is not None
+    assert prot_lig_file_xtc_unaligned is not None
     assert prot_lig_file_gro is not None
