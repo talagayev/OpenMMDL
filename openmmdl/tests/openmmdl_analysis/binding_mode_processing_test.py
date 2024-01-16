aac7f3ba372a8c96d75bd005c9ba9168af72eef9
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-16 16:35:28 +0100

    Update binding_mode_processing_test.py

--- openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
+++ openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
@@ -281,6 +281,19 @@ def sample_dataframe_bindingmode_processing_with_peptides():
     data['TARGET_IDX'][11] = 0
     data['RESNR_LIG'][11] = '[7, 8]'
 
+    data['FRAME'][12] = 4
+    data['Prot_partner'][12] = 'A'
+    data['INTERACTION'][12] = 'metal'
+    data['METAL_IDX'][12] = 401  # METAL_IDX for 'metal'
+    data['METAL_TYPE'][12] = 'Fe'  # Metal type
+    data['LOCATION'][12] = 'site1'  # Location
+    data['ACCEPTOR_IDX'][12] = 0
+    data['DON_IDX'][12] = 0
+    data['RESTYPE_LIG'][12] = 'A'  # Assuming 'A' as the RESTYPE_LIG for the metal interaction
+    data['TARGET_IDX'][12] = 401
+    data['COORDINATION'][12] = "site1"
+    data['RESNR_LIG'][12] = '501'
+
     return pd.DataFrame(data)
 
 
@@ -336,6 +349,7 @@ def test_gather_interactions_with_peptides(sample_dataframe_bindingmode_processi
         9: {9: 'A_[9_ 10]_Group4_pication'},
         10: {10: 'A_[9_ 10]_Group4_pication'},
         11: {11: 'B_[7, 8]_Group4_PI_saltbridge'},
+        12: {12: 'A_[3, 4]_Fe_site1_metal'},
     }
 
     # Check if the actual result matches the expected result
