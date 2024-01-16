4297c088e18d14e9b7e1cd56e5d639285330a7f3
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-16 14:58:37 +0100

    Update binding_mode_processing_test.py

--- openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
+++ openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
@@ -174,7 +174,7 @@ def sample_dataframe_bindingmode_processing_with_peptides():
         'RESTYPE_LIG': {0: '', 1: '', 2: '', 3: ''},
         'TARGET_IDX': {0: 0, 1: 0, 2: 0, 3: 0},
         'COORDINATION': {0: 0, 1: 0, 2: 0, 3: 0},
-        'RESNR_LIG' : {0: '101', 1: '501', 2: '501', 3: '501'}
+        'RESNR_LIG' : {0: '101', 1: '202', 2: '102', 3: '102'}
     }
     
     # Additional data for peptide interactions
