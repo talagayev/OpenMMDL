89df8e52ab7a0aef20013c62b69a56788b03027b
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-19 15:44:37 +0100

    Update binding_mode_processing_test.py

--- openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
+++ openmmdl/tests/openmmdl_analysis/binding_mode_processing_test.py
@@ -604,7 +604,7 @@ def sample_dataframe_it_peptides():
         'Prot_partner': ['62VAL', 'SER144', 'GLU321', 'ILE432', 'LEU248', 'SER300', 'TYR343', 'ILE178', 'PHE344', 'PHE754', 'LYS567', 'LYS567', 'HIS'],
         'LIGCARBONIDX': [101, 202, 155, 102, 501, 301, 467, 467, 398, 245, 228, 423, 256],
         'INTERACTION': ['hydrophobic', 'hbond', 'saltbridge', 'hydrophobic', 'halogen', 'hbond', 'waterbridge', 'waterbridge', 'pistacking', 'pication', 'pication', 'saltbridge', 'metal'],
-        'PROTISDON': [None, True, None, None, None, True, True, False, None, None, None, False, None],
+        'PROTISDON': [None, False, None, None, None, True, True, False, None, None, None, False, None],
         'ACCEPTORIDX': [None, 202, None, None, None, 301, None, None, None, None, None, None, None],
         'RESNR_LIG': [101, 202, 155, 102, 501, 301, 455, 467, 398, 245, 228, 423, 256],
         'DONORIDX': [None, None, None, None, None, None, None, None, None, None, None, None, None],
