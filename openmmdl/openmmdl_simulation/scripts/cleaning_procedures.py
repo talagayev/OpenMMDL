5efaa3c9596c3431a67f7638a6cf56ce140b50f0
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-22 11:09:16 +0100

    Update cleaning_procedures.py

--- openmmdl/openmmdl_simulation/scripts/cleaning_procedures.py
+++ openmmdl/openmmdl_simulation/scripts/cleaning_procedures.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from typing import List
 
+
 def cleanup(protein_name):
     """
     Cleans up the PDB Reporter Output File and MDTraj Files of the performed simulation
@@ -14,7 +15,7 @@ def cleanup(protein_name):
     Returns
     ----------
     None
-    """ 
+    """
     print("Cleaning Up :)")
     try:
         os.remove(f'output_{protein_name}')
@@ -23,7 +24,8 @@ def cleanup(protein_name):
     except FileNotFoundError:
         print("One or more files not found. Cleanup skipped.")
     print("Cleanup is done.")
-    
+
+
 def create_directory_if_not_exists(directory_path):
     """
     Create a directory if it doesn't exist, or overwrite it if already does.
@@ -36,12 +38,13 @@ def create_directory_if_not_exists(directory_path):
     Returns
     ----------
     None
-    """ 
+    """
     if not os.path.exists(directory_path):
         os.mkdir(directory_path)
     else:
         shutil.rmtree(directory_path)
         os.mkdir(directory_path)
+
 
 def copy_file(src, dest):
     """
@@ -57,9 +60,10 @@ def copy_file(src, dest):
     Returns
     ----------
     None
-    """ 
+    """
     if os.path.exists(src):
         shutil.copy(src, dest)
+
 
 def organize_files(source, destination):
     """
@@ -75,12 +79,15 @@ def organize_files(source, destination):
     Returns
     ----------
     None
-    """ 
+    """
     for file in source:
         if os.path.exists(file):
             os.rename(file, os.path.join(destination, os.path.basename(file)))
 
-def post_md_file_movement(protein_name:str, prmtop:str=None, inpcrd:str=None, ligands:List[str]=None):
+
+def post_md_file_movement(
+    protein_name: str, prmtop: str = None, inpcrd: str = None, ligands: List[str] = None
+):
     """
     Organizes and moves the files after the MD simulation to their respective directories.
     Parameters
@@ -97,7 +104,7 @@ def post_md_file_movement(protein_name:str, prmtop:str=None, inpcrd:str=None, li
     Returns
     ----------
     None
-    """ 
+    """
     # Create necessary directories
     create_directory_if_not_exists("Input_Files")
     create_directory_if_not_exists("MD_Files")
@@ -109,35 +116,78 @@ def post_md_file_movement(protein_name:str, prmtop:str=None, inpcrd:str=None, li
     create_directory_if_not_exists("Final_Output/All_Atoms")
     create_directory_if_not_exists("Final_Output/Prot_Lig")
     create_directory_if_not_exists("Checkpoints")
-
     # Move input files
     if ligands:
         for lig in ligands:
             copy_file(lig, "Input_Files")
             copy_file(lig, "Final_Output/All_Atoms")
             copy_file(lig, "Final_Output/Prot_Lig")
+
     copy_file(protein_name, "Input_Files")
     copy_file(prmtop, "Input_Files") if prmtop else None
     copy_file(inpcrd, "Input_Files") if inpcrd else None
-    
 
     # Organize pre-MD files
-    source_pre_md_files = ["prepared_no_solvent_", "solvent_padding_", "solvent_absolute_", "membrane_"]
+    source_pre_md_files = [
+        "prepared_no_solvent_",
+        "solvent_padding_",
+        "solvent_absolute_",
+        "membrane_",
+    ]
     destination_pre_md = "MD_Files/Pre_MD"
-    organize_files([f"{prefix}{protein_name}" for prefix in source_pre_md_files], destination_pre_md)
+    organize_files(
+        [f"{prefix}{protein_name}" for prefix in source_pre_md_files], destination_pre_md
+    )
 
     # Organize topology files after minimization and equilibration
     source_topology_files = ["Energyminimization_", "Equilibration_"]
     destination_topology = "MD_Files/Minimization_Equilibration"
-    organize_files([f"{prefix}{protein_name}" for prefix in source_topology_files], destination_topology)
+    organize_files(
+        [f"{prefix}{protein_name}" for prefix in source_topology_files],
+        destination_topology,
+    )
 
     # Organize simulation output files
-    organize_files([f"output_{protein_name}", "trajectory.dcd"], "MD_Files/MD_Output")
+    organize_files(
+        [f"output_{protein_name}", "trajectory.dcd"], "MD_Files/MD_Output"
+    )
 
     # Organize MDtraj and MDAnalysis files
-    organize_files(["centered_old_coordinates_top.pdb", "centered_old_coordinates.dcd", "centered_old_coordinates_top.gro", "centered_old_coordinates.xtc", "centered_traj_unaligned.dcd", "centered_traj_unaligned.xtc", "prot_lig_traj_unaligned.dcd", "prot_lig_traj_unaligned.xtc"], "MD_Postprocessing")
-    organize_files(["centered_top.pdb", "centered_traj.dcd", "centered_top.gro", "centered_traj.xtc"], "Final_Output/All_Atoms")
-    organize_files(["prot_lig_top.pdb", "prot_lig_traj.dcd", "prot_lig_top.gro", "prot_lig_traj.xtc"], "Final_Output/Prot_Lig")
+    organize_files(
+        [
+            "centered_old_coordinates_top.pdb",
+            "centered_old_coordinates.dcd",
+            "centered_old_coordinates_top.gro",
+            "centered_old_coordinates.xtc",
+            "centered_traj_unaligned.dcd",
+            "centered_traj_unaligned.xtc",
+            "prot_lig_traj_unaligned.dcd",
+            "prot_lig_traj_unaligned.xtc",
+        ],
+        "MD_Postprocessing",
+    )
+    organize_files(
+        [
+            "centered_top.pdb",
+            "centered_traj.dcd",
+            "centered_top.gro",
+            "centered_traj.xtc",
+        ],
+        "Final_Output/All_Atoms",
+    )
+    organize_files(
+        [
+            "prot_lig_top.pdb",
+            "prot_lig_traj.dcd",
+            "prot_lig_top.gro",
+            "prot_lig_traj.xtc",
+        ],
+        "Final_Output/Prot_Lig",
+    )
 
     # Organize checkpoint files
-    organize_files(["checkpoint.chk", "10x_checkpoint.chk", "100x_checkpoint.chk"], "Checkpoints")
+    organize_files(
+        ["checkpoint.chk", "10x_checkpoint.chk", "100x_checkpoint.chk"],
+        "Checkpoints",
+    )
+
