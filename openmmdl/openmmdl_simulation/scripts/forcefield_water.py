d8f9a246dc2c65bd0328420b636ea9ce9246c877
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-06-17 16:20:15 +0200

    Update forcefield_water.py
    gaff typo adjustment

--- openmmdl/openmmdl_simulation/scripts/forcefield_water.py
+++ openmmdl/openmmdl_simulation/scripts/forcefield_water.py
@@ -167,7 +167,7 @@ def generate_forcefield(protein_ff, solvent_ff, add_membrane, smallMoleculeForce
         forcefield = app.ForceField(protein_ff, solvent_ff)
     # If a ligand is present, a Forcefield with GAFF or SMIRNOFF will be created for the ligand
     if rdkit_mol is not None:
-        if smallMoleculeForceField == "Gaff":
+        if smallMoleculeForceField == "gaff":
             gaff = GAFFTemplateGenerator(
                 molecules=Molecule.from_rdkit(rdkit_mol, allow_undefined_stereo=True),
                 forcefield="gaff-2.11",
